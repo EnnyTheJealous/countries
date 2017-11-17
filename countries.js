f0eac4f13872be773c65e53642c6f1cd8a7d1c07
john rodriguez <johnr59545@students.laalliance.org>
2017-11-17 17:21:53 +0000

    done.

---
+++ countries.js
@@ -0,0 +1,15 @@
+var fourLetterCountries = [
+ "Chad",
+ "Cuba",
+ "Greenland",
+ "Iraq",
+ "Mali",
+ "Oman",
+ "India",
+    ]
+  fourLetterCountries.push("Fiji")
+  fourLetterCountries.splice(6,1)
+  fourLetterCountries.reverse(0)
+  fourLetterCountries.push()
+ console.log(fourLetterCountries)
+ 
